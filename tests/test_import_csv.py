dbf24ba82aa833cc7873c41678b90f8380ce5f05
BucKz96 <lefevre.maxime96@gmail.com>
2025-03-31 17:02:59 +0200

    fix: add print debugger

--- tests/test_import_csv.py
+++ tests/test_import_csv.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 def test_import_csv_success(client, app):
     source = os.path.join(os.getcwd(), 'data', 'test_meals.csv')
     dest = os.path.join(os.getcwd(), 'data', 'meals.csv')
@@ -9,6 +10,6 @@ def test_import_csv_success(client, app):
     response = client.post('/meals/import_csv', json={
         "file_path": "test_meals.csv"
     })
-
+    print("RESPONSE JSON:", response.json)
     assert response.status_code == 201
     assert "imported successfully" in response.json["message"]
